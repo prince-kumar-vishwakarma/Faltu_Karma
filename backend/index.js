98a384c4e6c1d2adf51d80f90c24a4cec42134f8
Abhishek Kumar <mintu12890551@gmail.com>
2025-04-16 16:44:53 +0530

    maked route

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,9 @@ dotenv.config()
 
 const app = express()
 
-
+app.get("/", (req, res) => {
+    res.send("Hello World")
+})
 const port = process.env.PORT || 3000
 app.listen(port, ()=>{
     console.log("Server running at http://localhost:3000")
