4cc6113443993b4ac34977822ba42e5a09e492dd
Abhishek Kumar <mintu12890551@gmail.com>
2025-04-17 10:45:40 +0530

    login,registor updated

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,72 +1,72 @@
-import mongoose from 'mongoose';
-import bcrypt from 'bcrypt';
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
-const studentSchema = new mongoose.Schema({
-    StudentID: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true
+const studentSchema = new mongoose.Schema(
+  {
+    studentID: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
     },
-    Name: {
-        type: String,
-        required: true,
-        trim: true
+    name: {
+      type: String,
+      required: true,
+      trim: true,
     },
-    ContactNumber: {
-        type: String,
-        required: true,
-        trim: true
+    contactNumber: {
+      type: String,
+      required: true,
+      trim: true,
     },
-    Email: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim: true
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
     },
-    Password: {
-        type: String,
-        required: true
+    password: {
+      type: String,
+      required: true,
     },
-    Course: {
-        type: String,
-        required: true
+    course: {
+      type: String,
+      required: true,
     },
-    Year: {
-        type: Number,
-        required: true,
-        min: 1,
-        max: 5
+    gender: {
+      type: String,
+      enum: ["male", "female", "other"],
+      required: true,
     },
-    Hostel: {
-        type: String,
-        required: true
+    dob: {
+      type: Date,
+      required: true,
     },
-    RefreshToken: {
-        type: String,
-        default: null
-    }
-}, {
-    timestamps: true
+
+    year: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+studentSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
 });
-
-
-studentSchema.pre('save', async function (next) {
-    if (!this.isModified('Password')) return next();
-    try {
-        const salt = await bcrypt.genSalt(10);
-        this.Password = await bcrypt.hash(this.Password, salt);
-        next();
-    } catch (err) {
-        next(err);
-    }
-});
-
 
 studentSchema.methods.comparePassword = async function (candidatePassword) {
-    return await bcrypt.compare(candidatePassword, this.Password);
+  return await bcrypt.compare(candidatePassword, this.password);
 };
 
-const Student = mongoose.model('Student', studentSchema);
+const Student = mongoose.model("Student", studentSchema);
 export default Student;
